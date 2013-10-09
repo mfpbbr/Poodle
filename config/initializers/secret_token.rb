2f4e94851215c62a4dd00d55d03d497c3ea52ca0
mitch cail <mitch.cail@gmail.com>
2013-10-08 22:32:00 -0400

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poodle::Application.config.secret_key_base = 'd6e82a9209edf0e7176125934eebdcce835b7f28b4a2a18a9a35324186a56408905daeb0a32b9f23db3078cca681dbe88cdd122d4de9afb23f26d0eafec181a5'
