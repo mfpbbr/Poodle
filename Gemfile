7ffc3b991cc7cda02ce7c3009342ab27cb087c1b
mitch cail <mitch.cail@gmail.com>
2013-10-13 13:25:26 -0400

    added devise to allow users to sign in and sign out

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'turbolinks'
 
 gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass', github: 'thomas-mcdonald/bootstrap-sass', branch: '3'
+gem 'devise', "~> 3.1.1"
 
 group :development, :test do
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Poodle::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # make a note to set this to the actual host name when its pushed to heroku
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
